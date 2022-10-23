4dbe3a9e949b63ed48b53959780681dad2b0d5df
MochammadIqbalAA <116489489+MochammadIqbalAA@users.noreply.github.com>
2022-10-23 17:21:38 +0700

    Delete booleanTrue.c

--- booleanTrue.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdbool.h>
-int main (int argc,char** argv){
-    bool perbandingan;
-
-    perbandingan = (2>5);
-    printf("Perbandingan 5>2 adalah %d", perbandingan);
-    return 0;
-}
